9c7272ccc935f78d7b9aa0be5dcbe4ce717dae31
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2024-06-17 14:55:14 -0700

    Implement parse_manifest_file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod manifest;
+mod tags;
+
 fn main() {
-    println!("Hello, world!");
 }
---
+++ src/manifest.rs
@@ -0,0 +1,84 @@
+use serde::{Serialize, Deserialize};
+use serde_yaml;
+use std::fs::read_to_string;
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct CopyOptions {
+    src: PathBuf,
+    dst: PathBuf,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Step {
+    copy: Vec<CopyOptions>,
+    tags: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Manifest {
+    steps: Vec<Step>,
+}
+
+/// Parse a coliru YAML manifest file
+pub fn parse_manifest_file(path: &Path) -> Result<Manifest, String> {
+    match read_to_string(path) {
+        Ok(raw) => match serde_yaml::from_str::<Manifest>(&raw) {
+            Ok(result) => Ok(result),
+            Err(why) => Err(why.to_string()),
+        }
+        Err(why) => Err(why.to_string()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(target_os = "linux")]
+    #[test]
+    fn parse_manifest_file_missing() {
+        let expected = "No such file or directory (os error 2)";
+        let actual = parse_manifest_file(Path::new("examples/0.yml"));
+        assert_eq!(actual, Err(String::from(expected)));
+    }
+
+    #[test]
+    fn parse_manifest_file_invalid() {
+        let expected = "steps[0]: missing field `copy` at line 4 column 5";
+        let actual = parse_manifest_file(Path::new("examples/1.yml"));
+        assert_eq!(actual, Err(String::from(expected)));
+    }
+
+    #[test]
+    fn parse_manifest_file_valid() {
+        let expected = Manifest {
+            steps: vec![
+                Step {
+                    copy: vec![
+                        CopyOptions{
+                            src: PathBuf::from("foo"),
+                            dst: PathBuf::from("~/foo")
+                        },
+                        CopyOptions{
+                            src: PathBuf::from("bar"),
+                            dst: PathBuf::from("~/test/bar")
+                        },
+                    ],
+                    tags: vec![String::from("a"), String::from("b")],
+                },
+                Step {
+                    copy: vec![
+                        CopyOptions{
+                            src: PathBuf::from("baz"),
+                            dst: PathBuf::from("/baz")
+                        },
+                    ],
+                    tags: vec![String::from("b"), String::from("c")],
+                }
+            ],
+        };
+        let actual = parse_manifest_file(Path::new("examples/2.yml"));
+        assert_eq!(actual, Ok(expected));
+    }
+}
